5da98b8dd7ee64dca9d63d31e3f607ea88a4fd85
Приходько Софія <sofiykapiryet@gmail.com>
2026-03-01 15:33:41 +0200

    done

--- src/domain/Exercise.java
+++ src/domain/Exercise.java
@@ -1,7 +1,15 @@
 package domain;
 
 public class Exercise {
-    public static float Calculate(int R){
-        return (float) (2*Math.PI*Math.sqrt(R));
+    public static int Calculate(int [] milit_numbs){
+        int counter = 0;        
+        for(int i = 0; i < 100000; i++){
+            String numb = Integer.toString(i);
+            if(numb.contains("4") || numb.contains("13")){
+                counter++;
+            }
+        }
+        return counter;
     }
 }
+
