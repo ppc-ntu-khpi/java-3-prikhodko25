016d34b5cda314c77be8b353772aa3a6ff7b8af2
Приходько Софія <sofiykapiryet@gmail.com>
2026-03-01 15:46:35 +0200

    done

--- src/test/TestResult.java
+++ src/test/TestResult.java
@@ -5,11 +5,10 @@ import domain.Exercise;
 public class TestResult {
 
     public static void main(String[] args) {
-        int[] milit_numbs = new int[100000];
-        for(int i = 0; i < milit_numbs.length; i++){
-            milit_numbs[i] = i;
-        }
+
+            String expr = "1 + 33 - 4 * 7";
+            float value = Exercise.Calculate(expr);
         
-        System.out.println("Кількість підрозділів, що містять числа = " + Exercise.Calculate(milit_numbs));
+            System.out.println("Результат виразу \"" + expr + "\" = " + value);
     }
 }
